1fc7ac27942b13a50343fbe6c0ac04f7187c0aaf
henryhuang <huangyijie1989@gmail.com>
2016-09-16 23:45:58 +0800

    travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,8 +4,6 @@ openssl aes-256-cbc -K $encrypted_adc0b7f2dba9_key -iv $encrypted_adc0b7f2dba9_i
 # Set the permission of the key
 chmod 600 ~/.ssh/id_rsa
 
-cat ~/.ssh/id_rsa
-
 # Start SSH agent
 eval $(ssh-agent)
 # Add the private key to the system
@@ -16,6 +14,6 @@ cp .travis/ssh_config ~/.ssh/config
 git config --global user.name "henryhuang"
 git config --global user.email h1886@outlook.com
 # Clone the repository
-git clone git@git.coding.net:henryhuang/huangyijie.git -b coding-pages .deploy_git
+# git clone git@git.coding.net:henryhuang/huangyijie.git -b coding-pages .deploy_git
 # Deploy to GitHub
 npm run deploy
